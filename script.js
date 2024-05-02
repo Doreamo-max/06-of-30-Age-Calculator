c2d9a178f8d84e5e00e962ff90bd311ba9b31ca1
Dheeraj Shrivastav <126507107+Doreamo-max@users.noreply.github.com>
2024-05-02 17:17:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,43 @@
-*{
-    margin: 0;
-    padding: 0;
-    font-family: 'Poppins' , sans-serif;
-    box-sizing: border-box;
-}
-.container{
-    width: 100%;
-    min-height: 100vh;
-    background: linear-gradient(135deg,#4203a4 ,#90bafc);
-    color: #fff;
-    padding: 10px;
-}
-.calculator{
-    width: 100%;
-    margin-top: 10%;
-    margin-left: 10%;
-    max-width: 600px;
-}
-.calculator h1{
-    font-size: 60px;
-}
-.calculator h1 span{
-   color:#ffff76 ;
-}
-.input-box{
-      margin: 40px 0;
-      padding: 35px;
-      background: rgba(255, 255, 255, 0.3);
-      display: flex;
-      align-items: center;
-}
-.input-box input{
-    flex: 1;
-    margin-right: 20px;
-    padding: 14px 20px;
-    border: 0;
-    outline: 0;
-    font-size: 18px;
-    border-radius: 5px;
-    position: relative;
-}
-.input-box button{
-    background: #ffff76;
-    border: 0;
-    outline: 0;
-    padding: 15px 30px;
-    font-weight: 500;
-    border-radius: 5px;
-    cursor: pointer;
-    color: #333;
-}  
-/* .input-box input::-webkit-calendar-picker-indicator{
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    width: auto;
-    height: auto;
-    position: absolute;
-    background-position: calc(100% -10px);
-    background-size: 30px;
-    cursor: pointer;
-}  */
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
+function calculateAge() {
+    let birthDate = new Date(userInput.value);
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
 
-#result{
-    font-size: 23px;
+    let today = new Date();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+    y3 = y2 - y1;
+
+    if (m2 >= m1) {
+        m3 = m2 - m1;
+    }
+    else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if (d2 >= d1) {
+        d3 = d2 - d1;
+    }
+    else {
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+    if (m3<0) {
+        m3=11;
+        y3--;
+    }
+    result.innerHTML =`Yor are <span>${y3}</span> years <span>${m3}</span> months and <span>${d3}</span> days old.`;
 }
-#result span{
-    color: #ffff76;
+
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
 }
